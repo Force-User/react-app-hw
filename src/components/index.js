c594d055846d3a55427508c9d7f5f398f841ee37
Anatoly Yakyshenko <anatoly.yakyshenko@gmail.com>
2021-04-06 11:13:57 +0300

    feat: add footer component

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,2 @@
 import App from "./app";
-export {App};
+export { App };
--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-import {App} from "./components";
+import { App } from "./components";
 import reportWebVitals from "./reportWebVitals";
 import image from "./assets/logo.svg";
 const store = {
-  user:{
-    firstName:"Jack",
-    lastName:"Sparrow",
+  user: {
+    firstName: "Jack",
+    lastName: "Sparrow",
   },
-  avatar:{
-    image:image,
-    alt:"logo",
+  avatar: {
+    image: image,
+    alt: "logo",
   },
-}
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App data={store}/>
+    <App data={store} />
   </React.StrictMode>,
   document.getElementById("root")
 );
