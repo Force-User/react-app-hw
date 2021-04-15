ee958d3ea717a77abccab777008ac5db98d37173
Anatoly Yakyshenko <anatoly.yakyshenko@gmail.com>
2021-04-15 14:55:38 +0300

    feat: add hw-2 react
    - add cards component
    - add card components
    - add api card

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,10 +1,11 @@
+import { Cards } from "./cards";
 import styles from "./main.module.scss";
 
 const Main = ({ avatar }) => {
-  console.log(avatar.image);
   return (
     <main className={styles.content}>
       <img src={avatar.image} alt={avatar.alt} className={styles.image} />
+      <Cards />
     </main>
   );
 };
